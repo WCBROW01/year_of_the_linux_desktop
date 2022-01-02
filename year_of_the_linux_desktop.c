19126622aab31b192ece0dd90778710d11d58de9
Will Brown <kyrosewoodgang@gmail.com>
2022-01-01 23:21:27 -0500

    Make text more descriptive.

--- year_of_the_linux_desktop.c
+++ year_of_the_linux_desktop.c
@@ -6,5 +6,5 @@ int main(void)
 	time_t current_time = time(NULL);
 	struct tm *time = localtime(&current_time);
 	int year_of_linux_desktop = time->tm_yday < 182 ? time->tm_year + 1900 : time->tm_year + 1901;
-	printf("%d\n", year_of_linux_desktop);
+	printf("The year of the Linux desktop will be %d.\n", year_of_linux_desktop);
 }
